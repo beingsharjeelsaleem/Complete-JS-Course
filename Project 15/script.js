d43265aa2f885f629514cab0c2847fe3afe2fe0c
Your Name <youremail@example.com>
2025-12-11 07:56:33 +0500

    JS Project 15 (Movie Search App)

---
+++ Project 15/script.js
@@ -0,0 +1,71 @@
+let form = document.querySelector("form");
+let movieContainer = document.querySelector("#movieContainer");
+let loader = document.querySelector("#loader");
+movieContainer.innerHTML = "Search for the movie to show up";
+
+apiKey = "3deba3e5";
+
+async function getMovie(movie) {
+  try {
+    loader.style.display = "flex";
+    movieContainer.style.display = "none";
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${apiKey}&s=${movie}`
+    );
+    if (!response.ok) {
+      throw new Error(movieData.Error);
+    }
+
+    const movieData = await response.json();
+    console.log(movieData.Search[0]);
+
+    movieContainer.innerHTML = "";
+
+    const card = document.createElement("div");
+    card.classList.add("movie-card");
+
+    const img = document.createElement("img");
+    img.classList.add("movie-poster");
+    img.src = movieData.Search[0].Poster;
+    img.alt = `${movieData.Search[0].Title} Poster`;
+
+    img.onload = () => {
+      loader.style.display = "none";
+      movieContainer.style.display = "flex";
+    };
+
+    const details = document.createElement("div");
+    details.classList.add("movie-details");
+
+    const title = document.createElement("div");
+    title.classList.add("movie-title");
+    title.textContent = movieData.Search[0].Title;
+
+    const year = document.createElement("div");
+    year.classList.add("movie-year");
+    year.textContent = movieData.Search[0].Year;
+
+    movieContainer.appendChild(card);
+    details.appendChild(title);
+    details.appendChild(year);
+    card.appendChild(img);
+    card.appendChild(details);
+  } catch (error) {
+    loader.style.display = "none";
+    movieContainer.style.display = "flex";
+    movieContainer.innerHTML = error;
+  }
+}
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  let searchInput = document.querySelector("#searchInput").value;
+
+  if (searchInput.trim() === "") {
+    alert("please fillout the required field before submitting the form");
+  }
+
+  getMovie(searchInput);
+  form.reset();
+});
