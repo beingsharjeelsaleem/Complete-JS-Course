04aab4fc1dc40d1848fd2d89cdef79deb5fc92a2
Your Name <youremail@example.com>
2025-12-18 04:38:50 +0500

    JS Project 20 (Stopwatch App)

---
+++ Project 20/script.js
@@ -0,0 +1,46 @@
+let timeDisplay = document.querySelector("#timeDisplay");
+let startBtn = document.querySelector("#startBtn");
+let pauseBtn = document.querySelector("#pauseBtn");
+let resetBtn = document.querySelector("#resetBtn");
+
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let timeInterval = null;
+
+startBtn.addEventListener("click", function () {
+  if (timeInterval) return;
+
+  timeInterval = setInterval(() => {
+    milliseconds++;
+
+    if (milliseconds >= 100) {
+      milliseconds = 0;
+      seconds++;
+    }
+    if (seconds >= 60) {
+      seconds = 0;
+      minutes++;
+    }
+
+    timeDisplay.textContent = `${minutes >= 10 ? minutes : "0" + minutes}:${
+      seconds >= 10 ? seconds : "0" + seconds
+    }:${milliseconds >= 10 ? milliseconds : "0" + milliseconds}`;
+  }, 10);
+});
+
+pauseBtn.addEventListener("click", function () {
+  clearInterval(timeInterval);
+  timeInterval = null;
+});
+
+resetBtn.addEventListener("click", function () {
+  clearInterval(timeInterval);
+  timeInterval = null;
+  minutes = 0;
+  seconds = 0;
+  milliseconds = 0;
+  timeDisplay.textContent = `${minutes >= 10 ? minutes : "0" + minutes}:${
+    seconds >= 10 ? seconds : "0" + seconds
+  }:${milliseconds >= 10 ? milliseconds : "0" + milliseconds}`;
+});
